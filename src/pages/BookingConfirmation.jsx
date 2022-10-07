cf4b7a6a7cc6bcae644962a4bf95dea9fbbce4ae
Himansu <35388216+himansu9805@users.noreply.github.com>
2022-10-08 03:03:37 +0530

    Added booking confirmation page

---
+++ src/pages/BookingConfirmation.jsx
@@ -0,0 +1,109 @@
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import Header from "../components/Header";
+import "../styles/BookingConfirmation.css";
+import { styled } from "@mui/material/styles";
+import React from "react";
+import { Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
+export function BookingConfirmation() {
+  return (
+    <div className="confirmationParent">
+      <Header />
+      <div className="confirmationContainer">
+        <div
+          style={{ marginTop: "30px", padding: "25px", width: "20%" }}
+          className="genericContainer confirmContainer"
+        >
+          <h1 className="confirmationText1">Your Booking ID is</h1>
+          <h1 className="confirmationID">OCT_07_1</h1>
+          <span className="confirmationNote">NOTE:</span>
+          <br />
+          <span className="confirmationNote">
+            1) Kindly carry your <strong>Aadhar Card</strong> with you when you
+            visit Jan Seva Kendra.
+          </span>
+          <br />
+          <span className="confirmationNote">
+            2) Please visit the Jan Seva Kendra only during your alloted slot.
+          </span>
+        </div>
+      </div>
+      <div className="confirmationContainer">
+        <div
+          style={{ marginTop: "30px", padding: "25px", width: "20%" }}
+          className="genericContainer confirmContainer"
+        >
+          <div style={{ marginBottom: "10px" }} className="confirmationRow">
+            <span>Don't know the address?</span>
+          </div>
+          <span>
+            <HtmlTooltip
+              title={
+                <React.Fragment>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "flex-start",
+                      alignItems: "center",
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      style={{ marginRight: "10px" }}
+                    >
+                      <path d="M0 0h24v24H0z" fill="none" />
+                      <path d="M20.5 3l-.16.03L15 5.1 9 3 3.36 4.9c-.21.07-.36.25-.36.48V20.5c0 .28.22.5.5.5l.16-.03L9 18.9l6 2.1 5.64-1.9c.21-.07.36-.25.36-.48V3.5c0-.28-.22-.5-.5-.5zM15 19l-6-2.11V5l6 2.11V19z" />
+                    </svg>
+                    <Typography color="inherit">Google Maps link</Typography>
+                  </div>
+                  <a href="https://www.google.com/maps/place/Mamlatdar+%26+Executive+Magistrate+Office,+Bharuch/@21.7092553,72.9886071,17z/data=!3m1!4b1!4m5!3m4!1s0x3be0274d2e7c3ecf:0xf08ae9ba639dfd53!8m2!3d21.7092554!4d72.9907957">
+                    Click here to show the office location on the Google Maps
+                  </a>{" "}
+                </React.Fragment>
+              }
+            >
+              <Button>
+                Mamlatdar & Executive Magistrate Office, Railway Colony,
+                Bharuch, Gujarat 392001
+              </Button>
+            </HtmlTooltip>
+          </span>
+        </div>
+        <div style={{ marginTop: "30px" }}>
+          <Link to="/portal" style={{ textDecoration: "none" }}>
+            <Button variant="contained">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24"
+                viewBox="0 0 24 24"
+                width="24"
+                fill="white"
+                style={{ marginRight: "10px" }}
+              >
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
+              </svg>
+              Go back
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const HtmlTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: "#f5f5f9",
+    color: "rgba(0, 0, 0, 0.87)",
+    maxWidth: 220,
+    fontSize: theme.typography.pxToRem(12),
+    border: "1px solid #dadde9",
+  },
+}));
--- src/pages/Portal.jsx
+++ src/pages/Portal.jsx
@@ -1,25 +1,25 @@
-import { Button, Select, FormControl, Grid, Typography } from '@mui/material'
-import { Box } from '@mui/system'
-import React from 'react'
-import DatePick from '../components/DatePick';
-import Header from '../components/Header'
-import Slots from '../components/Slots';
-import moment from 'moment'
-import AboutAppointment from '../components/AboutAppointment';
-import { UserContext } from '../services/UserContext';
-import { collection, getDocs, doc, addDoc } from '@firebase/firestore'
+import { Button, Select, FormControl, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import DatePick from "../components/DatePick";
+import Header from "../components/Header";
+import Slots from "../components/Slots";
+import moment from "moment";
+import AboutAppointment from "../components/AboutAppointment";
+import { UserContext } from "../services/UserContext";
+import { collection, getDocs, doc, addDoc } from "@firebase/firestore";
 import { db } from "../firebase";
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from "@mui/material/CircularProgress";
 import Snackbar from "@mui/material/Snackbar";
 import Alert from "@mui/material/Alert";
 function Portal() {
   const context = React.useContext(UserContext);
   const [service, setService] = React.useState(1);
   const [date, setDate] = React.useState(moment(new Date()));
-  const [loading, setLoading] = React.useState(false)
+  const [loading, setLoading] = React.useState(false);
   const [openSnackbar, setOpenSnackbar] = React.useState(false);
   const [snackbarText, setSnackbarText] = React.useState([]);
-  const [servicesList, setServicesList]  = React.useState([])
+  const [servicesList, setServicesList] = React.useState([]);
   const showSnackbar = (msg) => {
     setSnackbarText(msg);
     setOpenSnackbar(true);
@@ -28,24 +28,24 @@ function Portal() {
     setOpenSnackbar(false);
   };
   const validSlots = [
-    {time:'09 : 00 AM', isAvailable: true},
-    {time:'10 : 00 AM', isAvailable: true},
-    {time:'11 : 00 AM', isAvailable: true},
-    {time:'12 : 00 PM', isAvailable: true},
-    {time:'01 : 00 PM', isAvailable: true},
-    {time:'02 : 00 PM', isAvailable: true},
-    {time:'03 : 00 PM', isAvailable: true},
-    {time:'04 : 00 PM', isAvailable: true},
-    {time:'05 : 00 PM', isAvailable: true},
-    {time:'06 : 00 PM', isAvailable: true}
-  ] 
+    { time: "09 : 00 AM", isAvailable: true },
+    { time: "10 : 00 AM", isAvailable: true },
+    { time: "11 : 00 AM", isAvailable: false },
+    { time: "12 : 00 PM", isAvailable: true },
+    { time: "01 : 00 PM", isAvailable: true },
+    { time: "02 : 00 PM", isAvailable: true },
+    { time: "03 : 00 PM", isAvailable: true },
+    { time: "04 : 00 PM", isAvailable: true },
+    { time: "05 : 00 PM", isAvailable: true },
+    { time: "06 : 00 PM", isAvailable: true },
+  ];
   const [slot, setSlot] = React.useState(0);
   const handleChange = (event) => {
     setService(event.target.value);
   };
-  const bookAppointment = async() => {
-    const dateObj = moment(date)
-    setLoading(true)
+  const bookAppointment = async () => {
+    const dateObj = moment(date);
+    setLoading(true);
     try {
       /* await addDoc(collection(db, "Appointments"), {
         date: "" + dateObj.date() + "/" + (dateObj.month() + 1) + "/" + dateObj.year(),
@@ -53,116 +53,181 @@ function Portal() {
         slot_id: doc(db, "Slots", "Slot_01"),
         service_id: doc(db, "Services", "SID_00"),
       }) */
-      showSnackbar(["success","Your appointment has been booked successfully!"])
-      console.log("[BookAppointment] success")
+      showSnackbar([
+        "success",
+        "Your appointment has been booked successfully!",
+      ]);
+      console.log("[BookAppointment] success");
     } catch (err) {
-      showSnackbar(["error","Something went wrong while booking your appointment."])
-      console.log("[BookAppointment] error", err)
+      showSnackbar([
+        "error",
+        "Something went wrong while booking your appointment.",
+      ]);
+      console.log("[BookAppointment] error", err);
     }
-    setLoading(false)
-  }
+    setLoading(false);
+  };
 
-  React.useEffect(()=>{
-
-    async function getServices(){
+  React.useEffect(() => {
+    async function getServices() {
       const docsSnap = await getDocs(collection(db, "Services"));
-      const arr = []
-      docsSnap.forEach(doc => {
-        arr.push({id:doc.id, ...doc.data()})
+      const arr = [];
+      docsSnap.forEach((doc) => {
+        arr.push({ id: doc.id, ...doc.data() });
         console.log(doc.data());
-      })
-      setServicesList(arr)
+      });
+      setServicesList(arr);
     }
-    
-    setServicesList([])
-    // getServices()
-  }, [])
-console.log(servicesList)
+
+    // setServicesList([]);
+    getServices();
+  }, []);
+  console.log(servicesList);
   return (
     <Box>
       <Header />
       <Box p={2}>
-        <Grid container columnSpacing={ {md : 4}}>
-          <Grid item xs={12} md={3} sx={{ display: "flex",flexDirection: "column", alignItems:"center", justifyContent:"center", padding: 0 }}>
+        <Grid container columnSpacing={{ md: 4 }}>
+          <Grid
+            item
+            xs={12}
+            md={3}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: 0,
+            }}
+          >
             <div>
               <FormControl variant="standard" sx={{ minWidth: 250 }}>
-                <Typography styles={{ paddingLeft: "10px", paddingTop: "10px" }}><h4>Select the service</h4></Typography>
+                <Typography
+                  styles={{ paddingLeft: "10px", paddingTop: "10px" }}
+                >
+                  <h4>Select the service</h4>
+                </Typography>
                 <Select
                   labelId="demo-simple-select-standard-label"
                   id="demo-simple-select-standard"
                   value={service}
                   onChange={handleChange}
                   native
-                  variant="outlined">
+                  variant="outlined"
+                >
                   <optgroup label="Issuance">
-                    {
-                      servicesList.map((service, i)=>(
-                        <option value={i}>{service.name}</option>
-                      ))
-                    }
+                    {servicesList.map((service, i) => (
+                      <option value={i}>{service.name}</option>
+                    ))}
                   </optgroup>
                 </Select>
               </FormControl>
-              <div className='datePickContainer'>
-                <Typography><h4>Pick a date</h4></Typography>
-                <DatePick announceDate={(date)=>setDate(date)} />
+              <div className="datePickContainer">
+                <Typography>
+                  <h4>Pick a date</h4>
+                </Typography>
+                <DatePick announceDate={(date) => setDate(date)} />
               </div>
-              <div >
-                &nbsp;
-              </div>
+              <div>&nbsp;</div>
             </div>
           </Grid>
           <Grid item xs={12} md={6}>
-            <Slots validSlots={validSlots} announceSlot={(i)=>{
-              setSlot(i)
-            }} />
-            { servicesList.length>1&&<AboutAppointment service={servicesList[service]} />}
+            <Slots
+              validSlots={validSlots}
+              announceSlot={(i) => {
+                setSlot(i);
+              }}
+            />
+            {servicesList.length > 1 && (
+              <AboutAppointment service={servicesList[service]} />
+            )}
           </Grid>
           <Grid item xs={12} md={3}>
-            <div style={{marginTop:"30px"}} className='genericContainer confirmContainer'>
+            <div
+              style={{ marginTop: "30px" }}
+              className="genericContainer confirmContainer"
+            >
               <Typography sx={{ marginLeft: "15px", padding: 0 }}>
                 <h4>Appointment Confirmation</h4>
               </Typography>
-              <div className='confirmBody'>
-                <div className='confirmDate'>
-                  <Typography variant='h2' sx={{ fontWeight: 'bold', color: "#fff" }}>
-                    { date.date() }
+              <div className="confirmBody">
+                <div className="confirmDate">
+                  <Typography
+                    variant="h2"
+                    sx={{ fontWeight: "bold", color: "#fff" }}
+                  >
+                    {date.date()}
                   </Typography>
                 </div>
-                <div className='confirmFullDate'>
-                  { date.format('dddd') }, { date.format('MMMM') } { date.date() }
+                <div className="confirmFullDate">
+                  {date.format("dddd")}, {date.format("MMMM")} {date.date()}
                 </div>
-                <div className='confirmDateDetail'>
+                <div className="confirmDateDetail">
                   <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="18"><path d="M0 0h24v24H0z" fill="none" /><path fill="#333" d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z" /><path d="M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z" /></svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      width="18"
+                    >
+                      <path d="M0 0h24v24H0z" fill="none" />
+                      <path
+                        fill="#333"
+                        d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"
+                      />
+                      <path d="M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z" />
+                    </svg>
                   </span>
-                  <span style={{ marginLeft: "05px"}}>
-                    { validSlots[slot].time } { validSlots[slot+1]?.time ? <> -  {validSlots[slot+1]?.time}</> : <></> }
+                  <span style={{ marginLeft: "05px" }}>
+                    {validSlots[slot].time}{" "}
+                    {validSlots[slot + 1]?.time ? (
+                      <> - {validSlots[slot + 1]?.time}</>
+                    ) : (
+                      <></>
+                    )}
                   </span>
                 </div>
-                <div className='confirmDateDetail'>
+                <div className="confirmDateDetail">
                   <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="18"><g><path d="M0,0h24v24H0V0z" fill="none" /></g><g><path  fill="#333" d="M12,2c-4.2,0-8,3.22-8,8.2c0,3.32,2.67,7.25,8,11.8c5.33-4.55,8-8.48,8-11.8C20,5.22,16.2,2,12,2z M12,12c-1.1,0-2-0.9-2-2 c0-1.1,0.9-2,2-2c1.1,0,2,0.9,2,2C14,11.1,13.1,12,12,12z" /></g></svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      enable-background="new 0 0 24 24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      width="18"
+                    >
+                      <g>
+                        <path d="M0,0h24v24H0V0z" fill="none" />
+                      </g>
+                      <g>
+                        <path
+                          fill="#333"
+                          d="M12,2c-4.2,0-8,3.22-8,8.2c0,3.32,2.67,7.25,8,11.8c5.33-4.55,8-8.48,8-11.8C20,5.22,16.2,2,12,2z M12,12c-1.1,0-2-0.9-2-2 c0-1.1,0.9-2,2-2c1.1,0,2,0.9,2,2C14,11.1,13.1,12,12,12z"
+                        />
+                      </g>
+                    </svg>
                   </span>
-                  <span style={{ marginLeft: "05px"}}>
-                    Fafda Road, Jalebi Gali, Dusshera
+                  <span style={{ marginLeft: "05px" }}>
+                    Mamlatdar & Executive Magistrate Office, Railway Colony,
+                    Bharuch, Gujarat 392001
                   </span>
                 </div>
-                <br/>
+                <br />
                 <Button
                   variant="contained"
                   onClick={bookAppointment}
                   disabled={loading}
-                  style={{transition:"all 0.5s ease-in-out"}}
+                  style={{ transition: "all 0.5s ease-in-out" }}
                 >
-                  {!loading?
-                  <>Book Appointment</>
-                    : <>
-                      <span style={{opacity:0}}>PPPP</span>
+                  {!loading ? (
+                    <>Book Appointment</>
+                  ) : (
+                    <>
+                      <span style={{ opacity: 0 }}>PPPP</span>
                       <CircularProgress size="1rem" color="secondary" />
-                      <span style={{opacity:0}}>PPPP</span>
-                      </>
-                  }
+                      <span style={{ opacity: 0 }}>PPPP</span>
+                    </>
+                  )}
                 </Button>
               </div>
             </div>
@@ -183,7 +248,7 @@ console.log(servicesList)
         </Alert>
       </Snackbar>
     </Box>
-  )
+  );
 }
 
-export default Portal
+export default Portal;
