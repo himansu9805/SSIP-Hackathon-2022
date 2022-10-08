09f71ae953c34d86dc81065ba09d70f030592ad7
ajayraho4 <ajayraho4@gmail.com>
2022-10-08 10:45:42 +0530

    Final commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -59,29 +59,11 @@ function Header() {
               >
                 Appointment History
               </Link>
-<<<<<<< HEAD
-            </MenuItem>
-            <MenuItem
-              onClick={() => {
-                handleClose();
-                navigate("/");
-              }}
-            >
-              Logout
-            </MenuItem>
-||||||| 9bbd1ec
-            </MenuItem>}
-            <MenuItem onClick={()=>{
-              handleClose()
-              navigate('/')
-            }}>Logout</MenuItem>
-=======
             </MenuItem>
             <MenuItem onClick={()=>{
               handleClose()
               navigate('/')
             }}>Logout</MenuItem>
->>>>>>> 43546a10ccd63c07455df44aa4d6d24a70129b28
           </Menu>
         </div>
       </div>
